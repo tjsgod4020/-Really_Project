5c079490438b46b6bc874bd436c449233e8f4282
dlqud10 <dlqud10@naver.com>
2018-05-29 15:53:43 +0900

    get Exchange Rate data automatically when app opened

--- TBM/app/src/main/java/com/example/sun/tbm/MainActivity.java
+++ TBM/app/src/main/java/com/example/sun/tbm/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.sun.tbm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //환율 데이터 불러오기
+        try {
+            String logExRate = new Task().execute().get();
+            Log.i("환율데이터", logExRate);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 }
