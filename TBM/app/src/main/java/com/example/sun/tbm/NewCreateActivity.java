8de6c3ca1acd03666591b5b43abb346278ce016c
DESKTOP-T0GNUJP\SUN <tjsgod4020@gmail.com>
2018-06-04 00:30:30 +0900

    Revert "Revert "임시 여행 데이터 입력""
    This reverts commit 2f8df38af9b6593477b8e78309e5fc0dbaf0da2d.

--- TBM/app/src/main/java/com/example/sun/tbm/NewCreateActivity.java
+++ TBM/app/src/main/java/com/example/sun/tbm/NewCreateActivity.java
@@ -2,12 +2,36 @@ package com.example.sun.tbm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
 
 public class NewCreateActivity extends AppCompatActivity {
+    private Button button_NCs;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference mDatabase = database.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_create);
+
+        button_NCs = (Button) findViewById(R.id.button_NCs);
+
+        button_NCs.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                writeNewTrip("test", "tempName", "tempTimeS","TempTimee","TempArea", 100000,"TempMoney");
+            }
+        });
+    }
+
+    private void writeNewTrip(String tripID, String tripName, String tripTimeS, String tripTimeE, String tripArea, int tripBudget, String tripMoney){
+        newTrip trip = new newTrip(tripName, tripTimeS, tripTimeE , tripArea, tripBudget, tripMoney);
+
+        mDatabase.child("trip").child(tripID).setValue(trip);
     }
 }
+
