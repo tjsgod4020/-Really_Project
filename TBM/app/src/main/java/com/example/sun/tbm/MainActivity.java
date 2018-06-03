2f8df38af9b6593477b8e78309e5fc0dbaf0da2d
DESKTOP-T0GNUJP\SUN <tjsgod4020@gmail.com>
2018-06-04 00:29:38 +0900

    Revert "임시 여행 데이터 입력"
    This reverts commit 3b595cac8e2f010f8830903dfce4509db9fe30c6.

--- TBM/app/src/main/java/com/example/sun/tbm/MainActivity.java
+++ TBM/app/src/main/java/com/example/sun/tbm/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.sun.tbm;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void newButtonClick(View v) {
-        Intent intent = new Intent(getApplicationContext(), NewCreateActivity.class);
-        startActivity(intent);
+
+        //환율 데이터 불러오기
+        try {
+            String logExRate = new Task().execute().get();
+            Log.i("환율데이터", logExRate);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 }
