3b595cac8e2f010f8830903dfce4509db9fe30c6
DESKTOP-T0GNUJP\SUN <tjsgod4020@gmail.com>
2018-06-04 00:28:55 +0900

    임시 여행 데이터 입력

--- TBM/app/src/main/java/com/example/sun/tbm/MainActivity.java
+++ TBM/app/src/main/java/com/example/sun/tbm/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.sun.tbm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import java.util.concurrent.ExecutionException;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,17 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-
-        //환율 데이터 불러오기
-        try {
-            String logExRate = new Task().execute().get();
-            Log.i("환율데이터", logExRate);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-
+    public void newButtonClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), NewCreateActivity.class);
+        startActivity(intent);
     }
 }
